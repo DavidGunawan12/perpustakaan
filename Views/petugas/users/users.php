ab1474b982ffb8bdab06bbdce897af0f13823042
Dapiedd <gunawandavid419@gmail.com>
2023-03-12 08:05:27 +0700

    perpuss

---
+++ Views/petugas/users/users.php
@@ -0,0 +1,41 @@
+<?php
+
+// while($users = $perpus->listusers()){
+
+// }
+
+?>
+
+<a class="btn btn-success mb-3 shadow" href="dashboard.php?pages=users&act=tambah">Tambah Data users</a>
+
+<table class='table border shadow'>
+
+    <tr class="bg-primary text-light">
+        <th>No</th>
+        <th>Nama</th>
+        <th>Level</th>
+        <th>Opsi</th>
+    </tr>
+
+    <?php
+    $no = 1;
+    foreach($perpus->listusers() as $u){
+
+    
+    ?>
+    <tr>
+        <td><?= $no; ?></td>
+        <td><?= $u->username; ?></td>
+        <td><?= $u->level; ?></td>
+        <td>
+            <a class="btn btn-primary" href="dashboard.php?pages=users&act=ubah&id=<?= $u->user_id ?>">Ubah</a>
+            <a class="btn btn-danger" href="Routes/proses.php?act=hapususers&id=<?= $u->user_id ?>" onclick="return confirm('yakin user ingin dihapus?')">Hapus</a>
+        </td>
+    </tr>
+
+    <?php
+    $no++;
+    }
+    ?>
+
+</table>
